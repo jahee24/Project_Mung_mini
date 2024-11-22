e2b5c374b44a49694cd36b0790e7ff85577921ed
CoderJDan <coder.jangdaniel@gmail.com>
2024-11-22 14:56:09 +0900

    v1

---
+++ src/main/java/com/mung/square/ProjectMungMiniApplication.java
@@ -0,0 +1,13 @@
+package com.mung.square;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMungMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMungMiniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mung/square/ProjectMungMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mung.square;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMungMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
