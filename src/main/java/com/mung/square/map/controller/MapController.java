62717c74b8551f476bc1be1405950c0b22247aee
CoderJDan <coder.jangdaniel@gmail.com>
2024-11-28 20:15:32 +0900

    v7

--- src/main/java/com/mung/square/map/controller/MapController.java
+++ src/main/java/com/mung/square/map/controller/MapController.java
@@ -4,7 +4,6 @@ import com.mung.square.dto.MapDTO;
 import com.mung.square.map.service.MapService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
